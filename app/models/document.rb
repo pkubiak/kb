71d0d2ec6bda3578613e577f896343f7bc96694e
Paweł Kubiak <pawel.kubiak.it@gmail.com>
2018-02-18 14:18:05 +0100

    Estimate document reading time.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,7 @@
 class Document < ApplicationRecord
+  # Return reading time of document expressed in minutes. (Assuming 200 w/s)
+  # @return [Integer] number of minutes rounded-up.
+  def reading_time
+    ((content || '').scan(/\w+/).size + 199) / 200
+  end
 end
